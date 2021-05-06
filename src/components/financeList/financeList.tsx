3fbcb690f6bfc4866b3f6ee9183edccab9495c6a
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2021-05-07 01:50:27 +0300

    nominal

--- src/components/financeList/financeList.tsx
+++ src/components/financeList/financeList.tsx
@@ -9,11 +9,13 @@ const FinanceList: FC <FinanceListProps> = ({curs={}}) => {
 
 
   const itemList = Object.keys(curs).map((el)=>{
-
+    console.log(curs)
     return <FinanceToday key={curs[el].Previous*curs[el].Value} Name={curs[el].Name}
                          CharCode={curs[el].CharCode}
                          Value={curs[el].Value}
-                         Previous={curs[el].Previous}/>
+                         Previous={curs[el].Previous}
+                         Nominal={curs[el].Nominal}
+    />
   })
 
   return (
--- src/components/financeToday/financeToday.tsx
+++ src/components/financeToday/financeToday.tsx
@@ -7,15 +7,16 @@ export interface FinanceTodayProps {
   CharCode: string,
   Value: number,
   Previous: number,
+  Nominal: number
 }
 
-const FinanceToday: FC<FinanceTodayProps> = ({Name, CharCode, Value, Previous}) => {
+const FinanceToday: FC<FinanceTodayProps> = ({Name, CharCode, Value, Previous, Nominal}) => {
 
 
   let valueChange: priceChange = Value < Previous ? 'decreased' : 'increased'
   let previousChange: priceChange = Value > Previous ? 'decreased' : 'increased'
-  let difference = Math.round((Value- Previous)*10000)/10000
-  console.log( Value - Previous)
+  let difference = Math.round((Value - Previous) * 10000) / 10000
+  console.log(Value - Previous)
   return (
     <div className="finance">
       <div className="finance__currency">
@@ -25,13 +26,17 @@ const FinanceToday: FC<FinanceTodayProps> = ({Name, CharCode, Value, Previous}) 
 
       <div className="finance__current-value">
         <div className="finance__today">
-          <Number number={Value} difference={difference} priceChange={valueChange}/>
-           <div className="finance__caption">цена сегодня</div>
+          <Number number={Value} difference={difference}
+                  priceChange={valueChange}/>
+          <div className="finance__caption">цена сегодня</div>
         </div>
         <div className="finance__tomorrow">
           <Number number={Previous} size="font-small"/>
           <div className="finance__caption">цена вчера</div>
         </div>
+        <div className="finance__ratio">{
+          Nominal+ CharCode + ' / 1RUB'
+        }</div>
       </div>
 
     </div>)
